7bc09d816f06a1e0ba682b550e3cd9210519eea4
xuling94 <xuling94@outlook.com>
2018-01-23 14:06:55 +0800

    fix tab and structure

--- tests/test_udtsocket_server.py
+++ tests/test_udtsocket_server.py
@@ -10,28 +10,26 @@ FilePath = Struct('>260s')
 Int = Struct('>I')
 
 def remove_padding(s):
-	e = s.find('\0')
-	if e >= 0:
-		s = s[:e]
+    e = s.find('\0')
+    if e >= 0:
+        s = s[:e]
 
-	return s
+    return s
 
 class FileUDTServer(udt.udtsocket.UDTServer):
 
-	@coroutine
-	def on_data_ready(self, client):
-		filename = yield client.recv(FilePath.size)
-		filename = remove_padding(filename)
-		file_path = os.path.expanduser(
-			os.path.join('~', 'Desktop', filename)
-		)
-		size = os.path.getsize(file_path)
-		print "!", filename, size
-		client.send(Int.pack(size))
-		with open(file_path, 'rb') as f:
-			yield client.send_file(f, size)
+    @coroutine
+    def on_data_ready(self, client):
+        filename = yield client.recv(FilePath.size)
+        filename = remove_padding(filename)
+        file_path = os.path.join('.', 'tests', filename)
+        size = os.path.getsize(file_path)
+        print "!", filename, size
+        client.send(Int.pack(size))
+        with open(file_path, 'rb') as f:
+            yield client.send_file(f, size)
 
-		# client.send(data)
+        # client.send(data)
 
 s = FileUDTServer()
 s.bind(47008)
